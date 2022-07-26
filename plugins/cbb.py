494a35c3661b84757a7921abc36918dba47dfac9
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-07-26 10:07:54 +0700

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+# (©)Codexbotz
+# Recode By @mahadappa
+# Kalo clone Gak usah hapus ya kontol
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL, GROUP, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Channel : @kokbisadek\n • Owner : @mahadappa< • Link Repo: <a href='github.com/ReyyNada/ReyyFileSharing2'>Klik Disini</a></b>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔅 ᴛᴜᴛᴜᴘ 🔅", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
