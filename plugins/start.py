9910b0e1ae74cea2e2dc68b6e823f9de6c9ab9c3
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-08-05 12:02:36 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -116,15 +116,11 @@ async def start_command(client: Client, message: Message):
                 pass
     else:
         buttons = [
-            [InlineKeyboardButton("• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about")],
-            [
-                InlineKeyboardButton("•ᴄʜᴀɴᴇʟ•", url=client.invitelink1),
-                InlineKeyboardButton("•ᴄʜᴀɴᴇʟ•", url=client.invitelink2),
-            ],
-            [
-                InlineKeyboardButton("•ᴄᴏʙᴀ ʟᴀɢɪ•", callback_data="close"),
-            ],
-        ]
+                [
+                    InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data = "about"),
+                    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close")
+                ]
+            ]
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
